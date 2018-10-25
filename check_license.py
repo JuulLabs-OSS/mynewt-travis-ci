734c16f296b8cb4e4dbeece67d3891c10016b5a0
Fabio Utzig <utzig@apache.org>
2018-10-25 12:40:52 -0300

    Move GH access to backend
    Due to inability of forks to be able to use the Github API v3,
    an extra proxy will be used, running on runkit.com.
    
    A new XSTL to generate JSON output from RAT was added, that
    enables easier parsing of data.
    
    When parsing, new files with unknown licenses are output and
    parsed/sent to runkit/GH as comments and a status is written
    on the first commit with unknown files to fail the continuous
    integration.
    
    Signed-off-by: Fabio Utzig <utzig@apache.org>

--- check_license.py
+++ check_license.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -16,172 +17,242 @@
 # specific language governing permissions and limitations
 # under the License.
 
-import re
 import subprocess
-import json
 import os
-from urllib import request
+import requests
+import json
+import tarfile
+from datetime import datetime
+
+
+DEBUG = os.environ.get('DEBUG', 0)
+RAT_STYLESHEET = os.environ['HOME'] + '/ci/mynewt-rat-json.xsl'
 
 TRAVIS_REPO_SLUG = os.environ['TRAVIS_REPO_SLUG']
 TRAVIS_PULL_REQUEST = os.environ['TRAVIS_PULL_REQUEST']
 TRAVIS_COMMIT_RANGE = os.environ['TRAVIS_COMMIT_RANGE']
-GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
-HEADERS = {'Authorization': "token " + GITHUB_TOKEN}
-print(HEADERS)
-LICENSE_BOT_COMMENT_ID = "<!-- license-bot -->"
+LICENSE_BOT_ID = "<!-- license-bot -->"
 RAT_PATH = "apache-rat.jar"
 TARBALL_NAME = "archive.tgz"
-DIFF_FILTER = "A"
 RAT_URL = "https://repository.apache.org/content/repositories/snapshots/org" \
           "/apache/rat/apache-rat/0.14-SNAPSHOT/apache-rat-0.14-20181013" \
-          ".213930-1.jar "
+          ".213930-1.jar"
 
-# # Perform license check only on a PR
-# if TRAVIS_PULL_REQUEST == "false":
-#     print("Not a PR, exiting")
-#     exit(0)
+GH_STATUS_REPORTER_URL = "https://github-status-reporter-eb26h8raupyw.runkit.sh"
+GH_COMMENTER_URL = "https://github-commenter-l845aj3j3m9f.runkit.sh"
 
-# Download RAT jar file
-wget_cmd = "wget {} -O {}".format(RAT_URL, RAT_PATH)
-print("Executing: " + wget_cmd)
-output = subprocess.check_output(wget_cmd, shell=True).decode()
-print(output)
 
-# Get a list of new files added in current PR
-added_files_cmd = "git diff --no-commit-id --name-only -r " \
-                  "--diff-filter={} {}".format(DIFF_FILTER,
-                                               TRAVIS_COMMIT_RANGE)
-print("Executing: " + added_files_cmd)
-output = subprocess.check_output(added_files_cmd, shell=True).decode()
-files = output.splitlines()
+def install_rat():
+    '''
+    Download RAT
+    '''
+    r = requests.get(RAT_URL)
+    if r.status_code != 200:
+        exit(1)
+    open(RAT_PATH, 'wb').write(r.content)
+
+
+def tar_files(tarball, files):
+    '''
+    Combine files into an archive
+    '''
+    # RAT works only on archives and directories
+    with tarfile.open(TARBALL_NAME, 'w:gz') as tgz:
+        for file in files:
+            tgz.add(file)
+
+
+def run_rat(rat_path, tarball):
+    '''
+    Execute RAT on this archive and return the results
+    '''
+    rat_cmd = "java -jar {} --stylesheet {} {}".format(
+        rat_path, RAT_STYLESHEET, tarball)
+    if DEBUG:
+        print("Executing: " + rat_cmd)
+    output = subprocess.check_output(rat_cmd.split()).decode()
+    if DEBUG:
+        print(output)
+    return output
+
+
+def get_commit_list(commit_range):
+    '''
+    Get a list of commits in this PR, from older to newer
+    '''
+    first, last = commit_range.split('...')
+    cmd = "git log --pretty=format:%H {}..{}".format(first+'~', last)
+    if DEBUG:
+        print("Executing: " + cmd)
+    output = subprocess.check_output(cmd.split()).decode()
+    if DEBUG:
+        print("output: " + output)
+    # revert to get from older to newer
+    return output.splitlines()[::-1]
+
+
+def get_files_per_commits(commits):
+    '''
+    Accepts a list of commits (sha) and returns a map [added file -> sha]
+    '''
+    file_in_commit = {}
+    n = 0
+    while n < len(commits) - 1:
+        old, new = commits[n], commits[n+1]
+        cmd = "git log --no-commit-id --name-only --diff-filter=A {}..{}"\
+            .format(old, new)
+        if DEBUG:
+            print("Executing: " + cmd)
+        output = subprocess.check_output(cmd.split()).decode()
+        if DEBUG:
+            print(output)
+        for file in output.splitlines():
+            file_in_commit[file] = new
+        n += 1
+    return file_in_commit
+
+
+def get_added_files(commit_range):
+    '''
+    Get a list of new files added in current PR
+    '''
+    added_files_cmd = "git diff --no-commit-id --name-only -r " \
+                      "--diff-filter=A {}".format(commit_range)
+    if DEBUG:
+        print("Executing: " + added_files_cmd)
+    output = subprocess.check_output(added_files_cmd.split()).decode()
+    if DEBUG:
+        print("output: " + output)
+    return output.splitlines()
+
+
+def send_status(owner, repo, sha, state):
+    json = {
+        'owner': owner,
+        'repo': repo,
+        'target_url': os.environ['TRAVIS_BUILD_WEB_URL'],
+        'sha': sha,
+        'state': state,
+    }
+    r = requests.post(GH_STATUS_REPORTER_URL, json=json)
+    return r.status_code == 200
+
+
+def new_comment(owner, repo, pr, comment_body):
+    json = {
+        'owner': owner,
+        'repo': repo,
+        'number': pr,
+        'body': comment_body,
+    }
+    r = requests.post(GH_COMMENTER_URL, json=json)
+    return r.status_code == 200
+
+
+# Perform license check only on a PR
+if TRAVIS_PULL_REQUEST == "false":
+    print("Not a PR, exiting")
+    exit(0)
+
+added_files = get_added_files(TRAVIS_COMMIT_RANGE)
 
 # If there are no new files, there is no need to perform license check
-if not files:
+if not added_files:
     print("No new files in this PR, exiting")
+    # TODO: need to send success status as well?
     exit(0)
 
-# Combine these files into an archive
-# (RAT works only on archives and directories)
-tar_cmd = "tar -czf {} {}".format(TARBALL_NAME, ' '.join(files))
-print("Executing: " + tar_cmd)
-output = subprocess.check_output(tar_cmd, shell=True).decode()
-print(output)
+install_rat()
+tar_files(TARBALL_NAME, added_files)
+output = run_rat(RAT_PATH, TARBALL_NAME)
+rat = json.loads(output)
 
-# Execute RAT on this archive
-rat_cmd = "java -jar {} {}".format(RAT_PATH, TARBALL_NAME)
-print("Executing: " + rat_cmd)
-output = subprocess.check_output(rat_cmd, shell=True).decode()
-print(output)
+commits = get_commit_list(TRAVIS_COMMIT_RANGE)
 
-# Parse RAT summary and get meaningful information
-split_summary = re.split("^\*+$", output, flags=re.MULTILINE)
-summary = split_summary[1]
-# Find info on number of files with unknown licenses
-items = re.findall("(\d+) Unknown Licenses", summary, flags=re.MULTILINE)
-if not items:
-    print("No info on licenses, exiting")
+new_files_amount = int(rat['files_amount'])
+if new_files_amount == 0:
+    print("No new files were added, exiting")
     exit(0)
 
-licenses_count = int(items[0])
+unknown_amount = int(rat['unknown_amount'])
 
-if licenses_count == 0:
+# FIXME: known but category-x should be flagged
+if unknown_amount == 0:
     print("No unknown licenses, exiting")
     exit(0)
 
-assert(len(split_summary) >= 5)
-unapproved_licenses_list = split_summary[2]
-file_list = split_summary[4]
+file_in_commit = get_files_per_commits(commits)
 
-# Parse file list to get file names
-file_lines = file_list.splitlines()
-file_lines = [line.strip() for line in file_lines]
-file_lines = [line for line in file_lines if line]
+# Under unknown and files, look for each file and add an extra
+# key->val for the sha where it was added
 
-# There are 4 lines of file list header that explain the annotations
-assert(len(file_lines) > 4)
-file_list_header = file_lines[:4]
-files = file_lines[4:]
-split_files = [file.split() for file in files]
+unknown_files = rat['unknown']
+for file in unknown_files:
+    file['sha'] = file_in_commit[file['name']]
 
-# Get last commit id
-commits = TRAVIS_COMMIT_RANGE.split('...')
-last_commit_id = commits[1]
+new_files = rat['files']
+for file in new_files:
+    file['sha'] = file_in_commit[file['name']]
 
-# Format file list to include a link to a file on github
-commit_url = "https://github.com/{}/blob/{}/".format(TRAVIS_REPO_SLUG,
-                                                     last_commit_id)
-formatted_file_list = [
-    '{0: <6} <a href=\"{2}\">{1}</a>'.format(file[0], file[1],
-                                             commit_url + file[1])
-    for file in split_files
+commit_url = "https://github.com/{}/blob".format(TRAVIS_REPO_SLUG)
+unknown_files_fmt = [
+   '* <a href=\"{0}/{1}/{2}\">{2}</a>'.format(
+       commit_url, file_in_commit[file['name']], file['name'])
+   for file in unknown_files
 ]
 
-file_list_string = '\n'.join(file_list_header + formatted_file_list)
-noun_ending = 's' if licenses_count > 1 else ''
-licenses_count_str = '{} Unknown License{}'.format(licenses_count,
-                                                   noun_ending)
+new_files = [
+   '| {0: <6} | <a href=\"{1}/{2}/{3}\">{3}</a> |'.format(
+       file['type'], commit_url, file_in_commit[file['name']], file['name'])
+   for file in rat['files']
+]
 
-comment_format = """
-{}
-### {}
+ts = rat['timestamp']
+timestamp = datetime.strptime(ts[:-6], '%Y-%m-%dT%H:%M:%S')
 
-<details><summary>Details</summary>
-<p>
+# TODO: is it a good idea to send this formatted comment as text to
+#       the backend?
 
-<pre>
-{}
-------
-{}
-------
-{}
-</pre>
-
-</p>
-</details>
-""".format(LICENSE_BOT_COMMENT_ID,
-           licenses_count_str,
-           summary, unapproved_licenses_list,
-           file_list_string)
-
-# Get list of comments on current PR
-comment_url = "https://api.github.com/repos/{}/issues/{}/comments".format(
-    TRAVIS_REPO_SLUG, TRAVIS_PULL_REQUEST)
-req = request.Request(comment_url, headers=HEADERS)
-resp = request.urlopen(req)
-print("GET {}".format(comment_url))
-print("Status: {} {}".format(resp.status, resp.reason))
-json_comments_list = resp.read().decode()
-comments_list = json.loads(json_comments_list)
-
-
-# Find comments with license bot comment id in them
-bot_comments = []
-
-for comment in comments_list:
-    body = comment['body']
-    if LICENSE_BOT_COMMENT_ID in body:
-        bot_comments.append(comment['id'])
-
-# Delete these comments
-for comment_id in bot_comments:
-    comment_url = "https://api.github.com/repos/{}/issues/comments/{}".format(
-        TRAVIS_REPO_SLUG, comment_id)
-    req = request.Request(comment_url, headers=HEADERS)
-    req.get_method = lambda: 'DELETE'
-
-    resp = request.urlopen(req)
-    print("DELETE {}".format(comment_url))
-    print("Status: {} {}".format(resp.status, resp.reason))
+owner, repo = TRAVIS_REPO_SLUG.split("/")
 
 # Post a new comment
-comment_url = "https://api.github.com/repos/{}/issues/{}/comments".format(
-    TRAVIS_REPO_SLUG, TRAVIS_PULL_REQUEST)
-payload = {'body': comment_format}
+comment = """
+{}
 
-data = str(json.dumps(payload)).encode('utf-8')
-req = request.Request(comment_url, headers=HEADERS, data=data)
+## RAT Report ({})
 
-resp = request.urlopen(req)
-print("POST {}".format(comment_url))
-print("Status: {} {}".format(resp.status, resp.reason))
+## New files with unknown licenses
 
+{}
+
+<details>
+  <summary>Detailed analysis</summary>
+
+## New files in this PR
+
+| License | File |
+|---------|------|
+{}
+</details>
+
+""".format(LICENSE_BOT_ID, str(timestamp), "\n".join(unknown_files_fmt),
+           "\n".join(new_files))
+
+if DEBUG:
+    print("Comment body: ", comment)
+if not new_comment(owner, repo, TRAVIS_PULL_REQUEST, comment):
+    exit(1)
+
+# FIXME: check category-x?
+sha, state = None, None
+if unknown_amount == 0:
+    sha, state = commits[-1], 'success'
+else:
+    # FIXME: use oldest commit with unknown when sending status
+    for file in unknown_files:
+        commit = file_in_commit[file['name']]
+        sha, state = commit, 'failure'
+        break
+
+if not send_status(owner, repo, sha, state) or state == 'failure':
+    exit(1)
